95ec6270bc438100635ec33fb0414597d46aefa3
Walid Kurchied <Walid@ws-MacBook-Pro.local>
2020-05-18 03:09:34 +0100

    cleaned up master to make it smaller and will carry working from develop while keeping this just for prod

--- dist/Highlight/index.js
+++ dist/Highlight/index.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './styles.css';
+"use strict";
 
-const Highlight = ({
-  children,
-  color,
-  bg,
-  customBG,
-  customColor
-}) => {
-  let CustomComponent = 'span';
-  let classes = `highlighter highlighter-bg-${bg} highlighter-co-${color}`;
-  return /*#__PURE__*/React.createElement(CustomComponent, {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+require("./styles.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Highlight = function Highlight(_ref) {
+  var children = _ref.children,
+      color = _ref.color,
+      bg = _ref.bg,
+      customBG = _ref.customBG,
+      customColor = _ref.customColor;
+  var CustomComponent = 'span';
+  var classes = "highlighter highlighter-bg-".concat(bg, " highlighter-color-").concat(color);
+  return /*#__PURE__*/_react.default.createElement(CustomComponent, {
     className: classes
   }, children);
 };
 
 Highlight.propTypes = {
-  children: PropTypes.node.isRequired,
-  color: PropTypes.string.isRequired,
-  bg: PropTypes.string.isRequired
+  children: _propTypes.default.node.isRequired,
+  color: _propTypes.default.string.isRequired,
+  bg: _propTypes.default.string.isRequired
 };
 Highlight.defaultProps = {
   color: 'black',
   bg: 'yellow'
 };
-export default Highlight;
+var _default = Highlight;
+exports.default = _default;
